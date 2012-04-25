47ffed8e1a542898c0032fa382581ff19e651401
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-04-25 18:49:22 -0300

    Using sprite dimension with scarab.

--- prototype/init.lua
+++ prototype/init.lua
@@ -4,7 +4,7 @@ require "vec2"
 
 local scarab = sprite:new {
   position = vec2:new { 400, 300 },
-  size = vec2:new { 0.2,  0.2 }
+  dimension = vec2:new { 100, 100 }
 }
 
 scarab.image = love.graphics.newImage "data/scarab.png"
